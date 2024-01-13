6c20499d42b153c9e9d7379e58d646720c4602c2
sigmoidguo <sampsonguo302@gmail.com>
2024-01-13 09:34:03 +0800

    update

---
+++ daily/2182.cpp
@@ -0,0 +1,49 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+/**
+ * https://leetcode.cn/problems/construct-string-with-repeat-limit/?envType=daily-question&envId=2024-01-13
+ */
+class Solution {
+public:
+  string repeatLimitedString(string s, int repeatLimit) {
+    unordered_map<char, int> m;
+    for (int i = 0; i < s.size(); i++) {
+      m[s[i]]++;
+    }
+    sort(s.begin(), s.end());
+    reverse(s.begin(), s.end());
+    s.erase(unique(s.begin(), s.end()), s.end());
+    string ans;
+    for (int i = 0; i < s.size(); i++) {
+      // 如果该元素有值
+      while (m[s[i]] > 0) {
+        // 每n个元素一组
+        for (int j = 0; j < repeatLimit; j++) {
+          if (m[s[i]] > 0) {
+            ans += s[i];
+            m[s[i]]--;
+          } else {
+            break;
+          }
+        }
+        // 添加完之后，还需要找下一个能添加的元素
+        for (int j = i + 1; j < s.size(); j++) {
+          if (m[s[j]] > 0) {
+            ans += s[j];
+            m[s[j]]--;
+          } else {
+            break;
+          }
+        }
+      }
+    }
+    return s;
+  }
+};
+
+int main() {
+  cout << Solution().repeatLimitedString("cczazcc", 3) << endl;
+  cout << Solution().repeatLimitedString("aababab", 2) << endl;
+}
